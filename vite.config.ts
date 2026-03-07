566194bab550767d459dae7075d74fe4190a2dc0
admin <owenxuaozai@163.com>
2026-03-07 22:14:49 +0800

    allow host

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+  server: {
+    allowedHosts: ['test.aozai.top','aozai.top']
+  },
   build: {
     outDir: 'dist',
     target: 'es2020',
